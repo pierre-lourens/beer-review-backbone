3c0108f08201b0b4258b788921a1a7342ac4d658
amhayslip <amhayslip@gmail.com>
2018-11-21 10:35:39 -0500

    update addReview

--- collections/BeersCollection.js
+++ collections/BeersCollection.js
@@ -1,7 +1,7 @@
 var BeersCollection = Backbone.Collection.extend({
   model: BeerModel,
 
-  addBeer: function (name,style,abv,image_url) {
+  addBeer: function (name, style, abv, image_url) {
     this.add({
       name: name,
       style: style,
